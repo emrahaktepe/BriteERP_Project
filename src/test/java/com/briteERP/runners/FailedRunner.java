f3b014a12ee9841005b4e52cc78a13f9a5dd855d
zulfiyebiser <zlfdnbsr@gmail.com>
2020-01-06 20:42:33 -0600

    deleted features from FailedRunner class

--- src/test/java/com/briteERP/runners/FailedRunner.java
+++ src/test/java/com/briteERP/runners/FailedRunner.java
@@ -9,7 +9,7 @@ import org.junit.runner.RunWith;
 
 @CucumberOptions (
 
-        features = "@target/rerun.txt",
+        features = "",
         glue = "com/briteERP/step_definitions",
         plugin = {"html:target/default-cucumber-reports",
 
